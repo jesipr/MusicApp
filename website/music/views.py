66b3af6544cd26c3ba8476398e58b25fedda6283
Jesi <jesi.nieves@gmail.com>
2021-01-12 21:31:27 -0400

    Template Intro 14

--- website/music/views.py
+++ website/music/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template
-    return HttpResponse('')
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, album_id):
