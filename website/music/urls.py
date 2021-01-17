53b767bf4b59ce5390a8ebaa44a1953eb8d35a3e
Jesi <jesi.nieves@gmail.com>
2021-01-16 20:54:30 -0400

    Making the site a little simpler

--- website/music/urls.py
+++ website/music/urls.py
@@ -5,11 +5,9 @@ app_name = 'music'
 
 urlpatterns = [
     #/music/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 
     #/music/id/
-    path('<int:album_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 
-    #/music/id/favorite/
-    path('<int:album_id>/favorite/', views.favorite, name='favorite'),
 ]
